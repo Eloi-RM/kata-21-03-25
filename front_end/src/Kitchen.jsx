ba963222b11b935cbe9d37014815b9402f05a601
Eloi RM <eloimailmaynad@gmail.com>
2025-03-21 11:48:23 +0100

    FF

---
+++ front_end/src/Kitchen.jsx
@@ -0,0 +1,10 @@
+function Kitchen() {
+
+    return (
+        <div className='grid grid-cols-4 grid-rows-1 gap-4 px-4 py-4'>
+            <button className='hover:scale-90 transition-all cursor-grab text-white w-20 h-7 bg-emerald-700 rounded-2xl flex items-center justify-center'>
+                Acheter
+            </button>
+        </div>
+    );
+}
